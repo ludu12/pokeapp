8b75c3fb8bffdb7c74145e177f7f470486aacb30
Luke Dunscombe <dunscombe1@gmail.com>
2021-07-19 19:13:04 +0000

    Testing gitpod

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,6 @@ export const getStaticProps = getStaticPokemonByGen(1);
 
 export default function Home({ pokemon }) {
   // const { deck, clearDeck, downloadDeck, randomizeDeck, fetchDeck, toggleHandler, isSelected } = useDeck();
-
   return (
     <div className={classes.welcome}>
       <h1>
